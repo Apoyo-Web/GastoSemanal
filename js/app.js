de7ef830a604e212df3195ccbaeb5f1ae34770ef
Apoyo Web <mpienso@gmail.com>
2020-12-30 13:32:04 +0100

    entrar

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ eventListeners();
 
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+
+    formulario.addEventListener('submit', agregarGasto);
 }
 
 
@@ -25,6 +27,12 @@ class Presupuesto {
         this.gastos = [];
     }
 
+    nuevoGasto(gasto) {
+        this.gastos = [...this.gastos, gasto];
+
+        console.log(this.gastos);
+    }
+
 }
 
 class UI {
@@ -36,6 +44,42 @@ class UI {
         } = cantidad;
         document.querySelector('#total').textContent = presupuesto;
         document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje, tipo) {
+
+        //crear el div
+
+        const divMesanje = document.createElement('div');
+
+        divMesanje.classList.add('text-center', 'alert');
+
+
+        if (tipo === 'error') {
+            divMesanje.classList.add('alert-danger'); //Classe de bootstramp
+        } else {
+            divMesanje.classList.add('alert-success');
+        }
+
+
+        //agregar mensaje error
+
+        divMesanje.textContent = mensaje;
+
+        //insertar en HTML
+
+        document.querySelector('.primario').insertBefore(divMesanje, formulario);
+
+
+        //quitar del HTML
+
+        setTimeout(() => {
+
+            divMesanje.remove();
+
+        }, 2000);
+
+
     }
 
 }
@@ -52,17 +96,61 @@ const ui = new UI();
 
 function preguntarPresupuesto() {
 
-    const presupuestoUsuario = Number(prompt('¿Cuál es tu presupuesto?'));
+    presupuestoUsuario = 500;
 
-    if (presupuestoUsuario == "" || presupuestoUsuario == null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
+    // const presupuestoUsuario = Number(prompt('¿Cuál es tu presupuesto?'));
 
-        alert('Por favor, introduzca un presupuesto real en valor numérico')
-        window.location.reload();
-    }
+    // if (presupuestoUsuario == "" || presupuestoUsuario == null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
+
+    //     alert('Por favor, introduzca un presupuesto real en valor numérico')
+    //     window.location.reload();
+    // }
 
     presupuesto = new Presupuesto(presupuestoUsuario);
 
     ui.insertarPresupuesto(presupuesto);
 
     console.log(presupuesto);
+}
+
+
+//Añadir gastos
+
+function agregarGasto(e) {
+
+    e.preventDefault();
+
+
+    //Leer datos del formulario
+
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = Number(document.querySelector('#cantidad').value);
+
+    //Validacion
+
+    if (nombre === '' || cantidad === '') {
+
+        ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+
+        return;
+    } else if (cantidad <= 0 || isNaN(cantidad)) {
+
+        ui.imprimirAlerta('Cantidad no válida', 'error');
+
+        return;
+
+
+    }
+
+    const gasto = {
+        nombre,
+        cantidad,
+        id: Date.now()
+    };
+
+    presupuesto.nuevoGasto(gasto);
+
+    ui.imprimirAlerta('Gasto agregado Correctamente');
+    formulario.reset();
+
 }
